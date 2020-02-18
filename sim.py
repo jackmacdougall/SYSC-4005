700c9f3aaba9f3804e99d76e3131e318a934e8e3
Jack MacDougall <jackmacdougall@cmail.carleton.ca>
2020-02-17 22:53:16 -0500

    rearranged where the files are

--- sim.py
+++ sim.py
@@ -3,12 +3,12 @@ import entities
 
 def sim():
     simulation_env = simpy.Environment()
-    workstation1 = entities.WorkStation1(simulation_env)
-    workstation2 = entities.WorkStation2(simulation_env)
-    workstation3 = entities.WorkStation3(simulation_env)
+    workstation1 = entities.Workstation1(simulation_env)
+    workstation2 = entities.Workstation2(simulation_env)
+    workstation3 = entities.Workstation3(simulation_env)
     inspector1 = entities.Inspector1(simulation_env, workstation1, workstation2, workstation3)
     inspector2 = entities.Inspector2(simulation_env, workstation2, workstation3)
-    simulation_env.run(10000)
+    simulation_env.run(2400)
     
     print('Finished Simulation')
     print('{}{}'.format("Workstation 1 produced ",workstation1.productCount) + " of Product P1")
