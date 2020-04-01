54c6beef58ee8812042da5e860026694dc7a83f9
Jack MacDougall <jackmacdougall@cmail.carleton.ca>
2020-03-31 23:53:15 -0400

    updated verification

--- src/entities.py
+++ src/entities.py
@@ -10,31 +10,46 @@ queueC1W3WaitTimeList = []
 queueC2W2WaitTimeList = []
 queueC3W3WaitTimeList = []
 
+queueC1W1CapacityList = []
+queueC1W2CapacityList = []
+queueC1W3CapacityList = []
+queueC2W2CapacityList = []
+queueC3W3CapacityList = []
+
 queueC1W1WaitToggle = True
 queueC1W1StartTime1 = 0.0
 queueC1W1StartTime2 = 0.0
 queueC1W1ArrivalCount = 0
+queueC1W1DepartureCount = 0
+queueC1W1CapacityStart = 0.0
 
 queueC1W2WaitToggle = True
 queueC1W2StartTime1 = 0.0
 queueC1W2StartTime2 = 0.0
 queueC1W2ArrivalCount = 0
+queueC1W2DepartureCount = 0
+queueC1W2CapacityStart = 0.0
 
 queueC1W3WaitToggle = True
 queueC1W3StartTime1 = 0.0
 queueC1W3StartTime2 = 0.0
 queueC1W3ArrivalCount = 0
+queueC1W3DepartureCount = 0
+queueC1W3CapacityStart = 0.0
 
 queueC2W2WaitToggle = True
 queueC2W2StartTime1 = 0.0
 queueC2W2StartTime2 = 0.0
 queueC2W2ArrivalCount = 0
+queueC2W2DepartureCount = 0
+queueC2W2CapacityStart = 0.0
 
 queueC3W3WaitToggle = True
 queueC3W3StartTime1 = 0.0
 queueC3W3StartTime2 = 0.0
 queueC3W3ArrivalCount = 0
-
+queueC3W3DepartureCount = 0
+queueC3W3CapacityStart = 0.0
 
 
 
@@ -54,10 +69,14 @@ class Inspector1:
         self.action = env.process(self.run())
              
     def run(self):
-        global queueC1W1WaitToggle, queueC1W1StartTime1, queueC1W1StartTime2, queueC1W1ArrivalCount 
-        global queueC1W2WaitToggle, queueC1W2StartTime1, queueC1W2StartTime2, queueC1W2ArrivalCount
-        global queueC1W3WaitToggle, queueC1W3StartTime1, queueC1W3StartTime2, queueC1W3ArrivalCount
-        while True: 
+        global queueC1W1WaitToggle, queueC1W1StartTime1, queueC1W1StartTime2, queueC1W1ArrivalCount, queueC1W1CapacityList, queueC1W1CapacityStart
+        global queueC1W2WaitToggle, queueC1W2StartTime1, queueC1W2StartTime2, queueC1W2ArrivalCount, queueC1W2CapacityList, queueC1W2CapacityStart
+        global queueC1W3WaitToggle, queueC1W3StartTime1, queueC1W3StartTime2, queueC1W3ArrivalCount, queueC1W3CapacityList, queueC1W3CapacityStart
+        
+        queueC1W1CapacityStart = self.env.now
+        queueC1W2CapacityStart = self.env.now
+        queueC1W3CapacityStart = self.env.now
+        while True:  
             serviceTime = rng.inspector1_component1_rng()
             self.arrivalCount += 1
             yield self.env.timeout(float(serviceTime))
@@ -66,6 +85,10 @@ class Inspector1:
             print("Inspector 1 finished assembling component 1")
             if self.station1.buffer1.level <= self.station2.buffer1.level and self.station1.buffer1.level <= self.station3.buffer1.level:
                 yield self.station1.buffer1.put(1)
+                if(self.station1.buffer1.level == 0):
+                    queueC1W1CapacityList.append(tuple((self.station1.buffer1.level,self.env.now - queueC1W1CapacityStart, "put")))
+                else:
+                    queueC1W1CapacityList.append(tuple((self.station1.buffer1.level - 1, self.env.now - queueC1W1CapacityStart, "put")))
                 if queueC1W1WaitToggle:
                     queueC1W1StartTime1 = self.env.now
                     queueC1W1WaitToggle = False
@@ -73,9 +96,14 @@ class Inspector1:
                     queueC1W1StartTime2 = self.env.now
                     queueC1W1WaitToggle = True
                 queueC1W1ArrivalCount += 1
+                queueC1W1CapacityStart = self.env.now
                 print("Component 1 sent to Workstation 1")
             elif self.station2.buffer1.level <= self.station3.buffer1.level:
-                yield self.station2.buffer1.put(1)              
+                yield self.station2.buffer1.put(1)
+                if(self.station2.buffer1.level == 0):
+                    queueC1W2CapacityList.append(tuple((self.station2.buffer1.level, self.env.now - queueC1W2CapacityStart, "put")))
+                else:
+                    queueC1W2CapacityList.append(tuple((self.station2.buffer1.level - 1, self.env.now - queueC1W2CapacityStart, "put")))
                 if queueC1W2WaitToggle:
                     queueC1W2StartTime1 = self.env.now
                     queueC1W2WaitToggle = False
@@ -83,9 +111,14 @@ class Inspector1:
                     queueC1W2StartTime2 = self.env.now
                     queueC1W2WaitToggle = True    
                 queueC1W2ArrivalCount += 1
+                queueC1W2CapacityStart = self.env.now
                 print("Component 1 sent to Workstation 2")
             else:
                 yield self.station3.buffer1.put(1)
+                if(self.station3.buffer1.level == 0):
+                    queueC1W3CapacityList.append(tuple((self.station3.buffer1.level, self.env.now - queueC1W3CapacityStart, "put")))
+                else:
+                    queueC1W3CapacityList.append(tuple((self.station3.buffer1.level - 1, self.env.now - queueC1W3CapacityStart, "put")))
                 if queueC1W3WaitToggle:
                     queueC1W3StartTime1 = self.env.now
                     queueC1W3WaitToggle = False
@@ -93,6 +126,7 @@ class Inspector1:
                     queueC1W3StartTime2 = self.env.now
                     queueC1W3WaitToggle = True
                 queueC1W3ArrivalCount += 1
+                queueC1W3CapacityStart = self.env.now
                 print("Component 1 sent to Workstation 3")
         
         
@@ -111,8 +145,11 @@ class Inspector2:
         self.action = env.process(self.run())
         
     def run(self):
-        global queueC2W2WaitToggle, queueC2W2StartTime1, queueC2W2StartTime2, queueC2W2ArrivalCount
-        global queueC3W3WaitToggle, queueC3W3StartTime1, queueC3W3StartTime2, queueC3W3ArrivalCount
+        global queueC2W2WaitToggle, queueC2W2StartTime1, queueC2W2StartTime2, queueC2W2ArrivalCount, queueC2W2CapacityList, queueC2W2CapacityStart
+        global queueC3W3WaitToggle, queueC3W3StartTime1, queueC3W3StartTime2, queueC3W3ArrivalCount, queueC3W3CapacityList, queueC3W3CapacityStart
+        
+        queueC2W2CapacityStart = self.env.now
+        queueC3W3CapacityStart = self.env.now
         while True:
             if (random.randint(2, 3) == 2):
                 serviceTime = rng.inspector2_component2_rng()
@@ -122,6 +159,10 @@ class Inspector2:
                 self.componentCount = self.componentCount + 1
                 print("Inspector 2 finished assembling component 2") 
                 yield self.station2.buffer2.put(1)
+                if(self.station2.buffer2.level == 0):
+                    queueC2W2CapacityList.append(tuple((self.station2.buffer2.level, self.env.now - queueC2W2CapacityStart, "put")))
+                else:
+                    queueC2W2CapacityList.append(tuple((self.station2.buffer2.level - 1, self.env.now - queueC2W2CapacityStart, "put")))
                 if queueC2W2WaitToggle:
                     queueC2W2StartTime1 = self.env.now
                     queueC2W2WaitToggle = False
@@ -130,14 +171,19 @@ class Inspector2:
                     queueC2W2WaitToggle = True        
                 queueC2W2ArrivalCount += 1
                 print("Component 2 sent to Workstation 2")
+                queueC2W2CapacityStart = self.env.now
             else:
                 serviceTime = rng.inspector2_component3_rng()
                 self.arrivalCount = self.arrivalCount + 1   
-                yield self.env.timeout(float(serviceTime))
+                yield self.env.timeout(float(serviceTime))      
                 self.systemTime += float(serviceTime)
                 self.componentCount = self.componentCount + 1
                 print("Inspector 2 finished assembling component 3") 
                 yield self.station3.buffer3.put(1)
+                if(self.station3.buffer3.level == 0):          
+                    queueC3W3CapacityList.append(tuple((self.station3.buffer3.level, self.env.now - queueC3W3CapacityStart, "put")))
+                else:
+                    queueC3W3CapacityList.append(tuple((self.station3.buffer3.level - 1, self.env.now - queueC3W3CapacityStart, "put")))
                 if queueC3W3WaitToggle:
                     queueC3W3StartTime1 = self.env.now
                     queueC3W3WaitToggle = False
@@ -146,12 +192,12 @@ class Inspector2:
                     queueC3W3WaitToggle = True 
                 queueC3W3ArrivalCount += 1
                 print("Component 3 sent to Workstation 3")
+                queueC3W3CapacityStart = self.env.now
                
         
 class Workstation1:
     
-    def __init__(self, env):
-        global queueC1W1WaitToggle, queueC1W1StartTime1, queueC1W1StartTime2      
+    def __init__(self, env):    
         self.env = env
         self.name = "Workstation 1"
         self.product = "P1"
@@ -165,9 +211,15 @@ class Workstation1:
         self.action = env.process(self.run())
     
     def run(self):
-        global queueC1W1WaitToggle, queueC1W1StartTime1, queueC1W1StartTime2       
+        global queueC1W1WaitToggle, queueC1W1StartTime1, queueC1W1StartTime2, queueC1W1DepartureCount, queueC1W1CapacityStart  
         while True:
             yield self.buffer1.get(1)
+            if(self.buffer1.level == 2):
+                queueC1W1CapacityList.append(tuple((self.buffer1.level, self.env.now - queueC1W1CapacityStart, "get")))
+            else:
+                queueC1W1CapacityList.append(tuple((self.buffer1.level + 1, self.env.now - queueC1W1CapacityStart, "get")))
+            queueC1W1DepartureCount += 1
+            queueC1W1CapacityStart = self.env.now
             if queueC1W1WaitToggle:
                 queueC1W1WaitTimeList.append(self.env.now - queueC1W1StartTime2)          
             else:      
@@ -211,8 +263,14 @@ class Workstation2:
             print("Workstation 2 finished assembling product 2")         
  
     def C1W2Process(self):
-        global queueC1W2WaitToggle, queueC1W2StartTime1, queueC1W2StartTime2 
+        global queueC1W2WaitToggle, queueC1W2StartTime1, queueC1W2StartTime2, queueC1W2DepartureCount, queueC1W2CapacityList, queueC1W2CapacityStart
         yield self.buffer1.get(1)
+        if(self.buffer1.level == 2):
+            queueC1W2CapacityList.append(tuple((self.buffer1.level, self.env.now - queueC1W2CapacityStart, "get")))
+        else:
+            queueC1W2CapacityList.append(tuple((self.buffer1.level + 1, self.env.now - queueC1W2CapacityStart, "get")))
+        queueC1W2DepartureCount += 1
+        queueC1W2CapacityStart = self.env.now
         if queueC1W2WaitToggle:
             queueC1W2WaitTimeList.append(self.env.now - queueC1W2StartTime2)          
         else:      
@@ -220,8 +278,14 @@ class Workstation2:
             queueC1W2WaitToggle = True     
                         
     def C2W2Process(self):
-        global queueC2W2WaitToggle, queueC2W2StartTime1, queueC2W2StartTime2
+        global queueC2W2WaitToggle, queueC2W2StartTime1, queueC2W2StartTime2, queueC2W2DepartureCount, queueC2W2CapacityList, queueC2W2CapacityStart
         yield self.buffer2.get(1)
+        if(self.buffer2.level == 2):
+            queueC2W2CapacityList.append(tuple((self.buffer2.level, self.env.now - queueC2W2CapacityStart, "get")))
+        else:
+            queueC2W2CapacityList.append(tuple((self.buffer2.level + 1, self.env.now - queueC2W2CapacityStart, "get")))
+        queueC2W2DepartureCount += 1
+        queueC2W2CapacityStart = self.env.now
         if queueC2W2WaitToggle:
             queueC2W2WaitTimeList.append(self.env.now - queueC2W2StartTime2)          
         else:      
@@ -257,17 +321,29 @@ class Workstation3:
             print("Workstation 3 finished assembling product 3")  
                     
     def C1W3Process(self):
-        global queueC1W3WaitToggle, queueC1W3StartTime1, queueC1W3StartTime2
+        global queueC1W3WaitToggle, queueC1W3StartTime1, queueC1W3StartTime2, queueC1W3DepartureCount, queueC1W3CapacityList, queueC1W3CapacityStart
         yield self.buffer1.get(1)
+        if(self.buffer1.level == 2):
+            queueC1W3CapacityList.append(tuple((self.buffer1.level, self.env.now - queueC1W3CapacityStart, "get")))
+        else:
+            queueC1W3CapacityList.append(tuple((self.buffer1.level + 1, self.env.now - queueC1W3CapacityStart, "get")))        
+        queueC1W3DepartureCount += 1
+        queueC1W3CapacityStart = self.env.now
         if queueC1W3WaitToggle:
             queueC1W3WaitTimeList.append(self.env.now - queueC1W3StartTime2)          
         else:      
             queueC1W3WaitTimeList.append(self.env.now - queueC1W3StartTime1)    
-            queueC2W2WaitToggle = True     
+            queueC1W3WaitToggle = True     
         
     def C3W3Process(self):
-        global queueC3W3WaitToggle, queueC3W3StartTime1, queueC3W3StartTime2
+        global queueC3W3WaitToggle, queueC3W3StartTime1, queueC3W3StartTime2, queueC3W3DepartureCount, queueC3W3CapacityList, queueC3W3CapacityStart
         yield self.buffer3.get(1)
+        if(self.buffer3.level == 2):
+            queueC3W3CapacityList.append(tuple((self.buffer3.level, self.env.now - queueC3W3CapacityStart, "get")))
+        else:
+            queueC3W3CapacityList.append(tuple((self.buffer3.level + 1, self.env.now - queueC3W3CapacityStart, "get")))         
+        queueC3W3DepartureCount += 1
+        queueC3W3CapacityStart = self.env.now
         if queueC3W3WaitToggle:
             queueC3W3WaitTimeList.append(self.env.now - queueC3W3StartTime2)          
         else:      
--- src/inputVerification.py
+++ src/inputVerification.py
@@ -1,35 +1,121 @@
 import entities as en
 
 def littleLaw():
+ 
+    CalculateL("C1W1", en.queueC1W1CapacityList, en.queueC1W1DepartureCount)
+               
     total_wait_C1W1 = 0.0
     for wait in en.queueC1W1WaitTimeList:
         total_wait_C1W1 += wait
+   
     avg_wait_C1W1 = total_wait_C1W1/len(en.queueC1W1WaitTimeList)
     print("Average wait time for C1W1 = ", avg_wait_C1W1, " minutes")
     
-    arrival_rate_C1W1 = en.queueC1W1ArrivalCount/2400
-    print("Arrival rate for C1W1 = ", arrival_rate_C1W1, " components per minute")
+    arrival_rate_C1W1 = en.queueC1W1DepartureCount/2400
+    print("Arrival Rate for C1W1 = ", arrival_rate_C1W1, " components per minute")
+    
+    little_law_C1W1 = avg_wait_C1W1 * arrival_rate_C1W1
+    print("Little's Law calculation for C1W1 = ", little_law_C1W1)
+    
+    CalculateL("C1W2", en.queueC1W2CapacityList, en.queueC1W2DepartureCount)
     
     total_wait_C1W2 = 0.0
     for wait in en.queueC1W2WaitTimeList:
         total_wait_C1W2 += wait
     avg_wait_C1W2 = total_wait_C1W2/len(en.queueC1W2WaitTimeList)
-    print("Average wait time for C1W2 = ", avg_wait_C1W2, " minutes")    
+    print("Average wait time for C1W2 = ", avg_wait_C1W2, " minutes")  
+    
+    arrival_rate_C1W2 = en.queueC1W2DepartureCount/2400
+    print("Arrival Rate for C1W2 = ", arrival_rate_C1W2, " components per minute")
+        
+    little_law_C1W2 = avg_wait_C1W2 * arrival_rate_C1W2
+    print("Little's Law calculation for C1W2 = ", little_law_C1W2)    
 
+    CalculateL("C1W3", en.queueC1W3CapacityList, en.queueC1W3DepartureCount)
+    
     total_wait_C1W3 = 0.0
     for wait in en.queueC1W3WaitTimeList:
         total_wait_C1W3 += wait
     avg_wait_C1W3 = total_wait_C1W3/len(en.queueC1W3WaitTimeList)
     print("Average wait time for C1W3 = ", avg_wait_C1W3, " minutes")
+       
+    arrival_rate_C1W3 = en.queueC1W3DepartureCount/2400
+    print("Arrival Rate for C1W3 = ", arrival_rate_C1W3, " components per minute")
+        
+    little_law_C1W3 = avg_wait_C1W3 * arrival_rate_C1W3
+    print("Little's Law calculation for C1W3 = ", little_law_C1W3)    
+
+    CalculateL("C2W2", en.queueC2W2CapacityList, en.queueC2W2DepartureCount)
     
     total_wait_C2W2 = 0.0
-    for wait in en.queueC1W1WaitTimeList:
+    for wait in en.queueC2W2WaitTimeList:
         total_wait_C2W2 += wait
     avg_wait_C2W2 = total_wait_C2W2/len(en.queueC2W2WaitTimeList)
     print("Average wait time for C2W2 = ", avg_wait_C2W2, " minutes")
     
+    arrival_rate_C2W2 = en.queueC2W2DepartureCount/2400
+    print("Arrival Rate for C2W2 = ", arrival_rate_C2W2, " components per minute")
+            
+    little_law_C2W2 = avg_wait_C2W2 * arrival_rate_C2W2
+    print("Little's Law calculation for C2W2 = ", little_law_C2W2)       
+    
+    CalculateL("C3W3", en.queueC3W3CapacityList, en.queueC3W3DepartureCount)
+    
     total_wait_C3W3 = 0.0
-    for wait in en.queueC1W1WaitTimeList:
+    for wait in en.queueC3W3WaitTimeList:
         total_wait_C3W3 += wait
     avg_wait_C3W3 = total_wait_C3W3/len(en.queueC3W3WaitTimeList)
-    print("Average wait time for C2W2 = ", avg_wait_C3W3, " minutes")
+
+    print("Average wait time for C3W3 = ", avg_wait_C3W3, " minutes")
+    
+    arrival_rate_C3W3 = en.queueC3W3DepartureCount/2400
+    print("Arrival Rate for C3W3 = ", arrival_rate_C3W3, " components per minute")
+            
+    little_law_C3W3 = avg_wait_C3W3 * arrival_rate_C3W3
+    print("Little's Law calculation for C3W3 = ", little_law_C3W3)   
+
+    #print(len(en.queueC1W1WaitTimeList))
+    #print(len(en.queueC3W3CapacityList))
+    #print(en.queueC3W3DepartureCount)
+    
+    for e in en.queueC3W3CapacityList:
+        print(e[0], e[1], e[2])
+     
+
+def CalculateL(name, lst, threshold):
+    oneTime = 0.0
+    twoTime = 0.0
+    total = 0.0
+    counter = 0
+    
+    #for entry in lst:      
+        #if counter < threshold:
+            #if entry[0] == 1:
+                #oneTime += entry[1]
+            #elif entry[0] == 2:
+                #twoTime += entry[1]
+            #total += entry[1]
+        #if entry[2] == "get":
+            #counter += 1        
+    #if(total > 0.0):          
+        #avg = (((oneTime)+(twoTime * 2)))/total
+        #print("L for ", name ," = ", avg)   
+    #else:
+        #print("L for ", name ," = 0.0")
+    
+    for entry in lst:  
+        if entry[0] == 1:
+            oneTime += entry[1]
+        elif entry[0] == 2:
+            twoTime += entry[1]
+        total += entry[1]
+        if entry[2] == "get":
+            counter += 1  
+        if counter >= threshold:
+            break
+        
+    if(total > 0.0):          
+        avg = (((oneTime)+(twoTime * 2)))/total
+        print("L for ", name ,"= ", avg)   
+    else:
+        print("L for ", name ,"= 0.0")    
